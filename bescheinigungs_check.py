037f886635d08032225f0cc46207ffa8db038ced
maehw <maehw@posteo.de>
2023-08-12 18:10:13 +0200

    Refactoring: create class, let gui.py use that class, remove obsolete check_pdf.py

---
+++ bescheinigungs_check.py
@@ -0,0 +1,115 @@
+class BescheinigungsCheck:
+    QUERY_URL = "https://nami.dpsg.de/ica/sgb-acht-bescheinigung-pruefen"
+
+    # only use the following two to check failure and success
+    MSG_SUCCESS = '<p class="success-msg">'
+    MSG_FAILURE = '<p class="failure-msg">'
+
+    def __init__(self, firstnames, surname, birthdate, number):
+        self.firstnames = firstnames
+        self.surname = surname
+        self.birthdate = birthdate
+        self.number = number
+        self.check_input_data()
+        self.check_status = self.check()
+
+    def get_firstnames(self):
+        return self.firstnames
+
+    def get_surname(self):
+        return self.surname
+
+    def get_birthdate(self):
+        return self.birthdate
+
+    def get_id(self):
+        return self.number
+
+    def get_check_status(self):
+        return self.check_status
+
+    def check(self, timeout=5):
+        import requests
+
+        r = requests.post(BescheinigungsCheck.QUERY_URL,
+                          {
+                            'fzNummer': self.number,
+                            'vorname': self.firstnames,
+                            'nachname': self.surname,
+                            'geburtsdatum': self.birthdate
+                          },
+                          timeout=timeout)
+
+        if r.status_code == requests.codes.ok:
+            if (BescheinigungsCheck.MSG_SUCCESS in r.text) and not (BescheinigungsCheck.MSG_FAILURE in r.text):
+                return True
+        return False
+
+    def __repr__(self):
+        return f"Vorname(n): {self.firstnames}\n" \
+               f"Nachname: {self.surname}\n" \
+               f"Geb.datum: {self.birthdate}\n" \
+               f"ID: {self.number}\n" \
+               f"Check-Status: {self.check_status}"
+
+    def check_input_data(self):
+        import re
+        if not self.firstnames:
+            raise ValueError('Invalid first name(s)')
+        if not self.surname:
+            raise ValueError('Invalid surname')
+
+        if not self.birthdate:
+            raise ValueError('Invalid birthdate')
+        elif not re.match(r"\d{2}\.\d{2}\.\d{4}", self.birthdate):
+            raise ValueError(f"Unexpected format of birthdate (got: '{self.birthdate}')")
+
+        if not self.number:
+            raise ValueError('Invalid ID')
+        elif not re.match(r"\d{5}-\d{4,5}", self.number):
+            raise ValueError(f"Unexpected format of ID (got: '{self.number}')")
+
+    @classmethod
+    def from_pdf_file(cls, filename):
+        from pypdf import PdfReader
+
+        reader = PdfReader(filename)
+        text = reader.pages[0].extract_text()
+        lines = text.splitlines()
+
+        expected_text = [
+            '',
+            'Bescheinigung über die Einsichtnahme in das erweiterte ',
+            'Führungszeugnis',
+            'Hiermit bestätigen wir, dass',
+            'geboren am  ',
+            'wohnhaft in ',
+            'am   uns ein erweitertes Führungszeugnis vorgelegt hat. Das  ',
+            'Führungszeugnis  mit  dem  Datum  vom   wurde  durch  uns  ',
+            'eingesehen und enthielt im Sinne des §72a SGB VIII keine Eintragungen.',
+            'Diese  Bestätigung  wurde  automatisch  generiert  und  ist  auch  ohne  ',
+            'Unterschrift gültig. Die Echtheit dieses Dokument kann auf der folgenden  ',
+            'Webseite geprüft werden.',
+            'Identifikationsnummer: ',
+            ' Fon: ',
+            ' Fax: ',
+            ' www.dpsg.de',
+            ' Rechtsträger: Bundesamt Sankt Georg e.V.'
+        ]
+
+        # sanity check: check if expected text matches (ignoring whitespaces in front and back)
+        for i in range(0, len(expected_text)):
+            if expected_text[i].strip() != lines[i].strip():
+                raise ValueError('Expected PDF content format does not match. Format may have changed.')
+                # when this exception is thrown, please open an issue on GitHub!
+
+        # extract the interesting parts:
+        offset = len(expected_text)
+        number = lines[offset + 2]
+        name = lines[offset + 4].split(' ')
+        surname = name[-1]  # assume that the last name does not contain a space and equals the last element
+        firstnames = ' '.join(name[:-1])  # all other names go into firstname
+        birthdate = lines[offset + 9]
+
+        print(f"From PDF file: {filename}")
+        return cls(firstnames, surname, birthdate, number)
--- gui.py
+++ gui.py
@@ -1,16 +1,9 @@
+from bescheinigungs_check import BescheinigungsCheck
 import os
-import requests
 import datetime
-from tkinter import TOP, CENTER, BOTTOM, Label, font
+from tkinter import TOP, BOTTOM, Label, font
 from tkinterdnd2 import *
-from pypdf import PdfReader
 
-
-QUERY_URL = "https://nami.dpsg.de/ica/sgb-acht-bescheinigung-pruefen"
-
-# only use the following two to check failure and success
-MSG_SUCCESS = '<p class="success-msg">'
-MSG_FAILURE = '<p class="failure-msg">'
 
 window_width = 550
 window_height = 350
@@ -18,51 +11,6 @@ title = 'SGB VIII Check'
 color_failed = '#FF681E'
 color_success = '#069C56'
 color_error = '#FF980E'
-
-expected_text = [
-    '',
-    'Bescheinigung über die Einsichtnahme in das erweiterte ',
-    'Führungszeugnis',
-    'Hiermit bestätigen wir, dass',
-    'geboren am  ',
-    'wohnhaft in ',
-    'am   uns ein erweitertes Führungszeugnis vorgelegt hat. Das  ',
-    'Führungszeugnis  mit  dem  Datum  vom   wurde  durch  uns  ',
-    'eingesehen und enthielt im Sinne des §72a SGB VIII keine Eintragungen.',
-    'Diese  Bestätigung  wurde  automatisch  generiert  und  ist  auch  ohne  ',
-    'Unterschrift gültig. Die Echtheit dieses Dokument kann auf der folgenden  ',
-    'Webseite geprüft werden.',
-    'Identifikationsnummer: ',
-    ' Fon: ',
-    ' Fax: ',
-    ' www.dpsg.de',
-    ' Rechtsträger: Bundesamt Sankt Georg e.V.'
-]
-
-
-# TODO/FIXME: don't repeat yourself (DRY); this code has been copied from `check.py` (consider this being a "quick hack")
-def query_efz_status(input_data):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    complete = all(val != "" for val in input_data.values())
-
-    if not complete:
-        status = "Eingangsdaten unvollständig"  # TODO: could be more precise about what is missing
-    else:
-        status = "Ungültig oder Abfrage fehlerhaft"
-
-        r = requests.post(QUERY_URL, input_data, timeout=5)
-
-        try:
-            if r.status_code == requests.codes.ok:
-                if (MSG_SUCCESS in r.text) and not (MSG_FAILURE in r.text):
-                    status = "Gültig"
-        except:
-            # swallow bad requests for robustness (so that the script won't stop for a single entry)
-            # TODO: narrow it down and don't keep a bare exception handler this broad!
-            pass
-
-    return status, timestamp
 
 
 def get_path(event):
@@ -73,60 +21,27 @@ def get_path(event):
     status_label.configure(text='')
     status_label.configure(text='Wird geprüft.')
 
-    reader = PdfReader(full_file_name)
-    text = reader.pages[0].extract_text()
+    check = BescheinigungsCheck.from_pdf_file(full_file_name)
+    efz_status = check.get_check_status()
 
-    try:
-        assert QUERY_URL in text, "Could not find the QUERY_URL in the PDF's text. Unexpected format."
+    number = check.get_id()
+    firstnames = check.get_firstnames()
+    surname = check.get_surname()
+    birthdate = check.get_birthdate()
 
-        lines = text.splitlines()
+    details_label.configure(text=f"{number} / {firstnames} {surname}, geb. am {birthdate}")
 
-        # for debugging only:
-        # print(lines)
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # sanity check: check if expected text matches (ignoring whitespaces in front and back)
-        for i in range(0, len(expected_text)):
-            assert expected_text[i].strip() == lines[i].strip(), \
-                "Expected PDF content format does not match. Format may have changed."
-            # when this assertion breaks, please open an issue on GitHub!
+    output_format = f"[{timestamp}] {firstnames} {surname}   {efz_status}"
+    print(output_format)
 
-        # for debugging only:
-        # for i in range(len(expected_text), len(lines)):
-        #    print(lines[i])
-
-        offset = len(expected_text)
-        fznumber = lines[offset + 2]
-        name = lines[offset + 4].split(' ')
-        surname = name[-1]  # assume that the last name does not contain a space and equals the last element
-        firstname = ' '.join(name[:-1])  # all other names go into firstname
-        birthdate = lines[offset + 9]
-
-        request_data = {
-            'fzNummer': fznumber,
-            'vorname': firstname,
-            'nachname': surname,
-            'geburtsdatum': birthdate
-        }
-
-        details_label.configure(text=f"{fznumber} / {firstname} {surname}, geb. am {birthdate}")
-
-        # For debugging only:
-        # print(request_data)
-
-        efz_status, timestamp = query_efz_status(request_data)
-
-        output_format = f"[{timestamp}] {request_data.get('vorname')} " \
-                        f"{request_data.get('nachname')}   {efz_status}"
-        print(output_format)
-
-        status_label.configure(text=efz_status)
-        if efz_status == 'Gültig':
-            root.configure(background=color_success)
-        else:
-            root.configure(background=color_failed)
-    except:
-        status_label.configure(text='Prüfung nicht möglich.')
-        root.configure(background=color_error)
+    if efz_status:
+        status_label.configure(text='Gültig.')
+        root.configure(background=color_success)
+    else:
+        status_label.configure(text='Ungültig oder Prüfung fehlgeschlagen.')
+        root.configure(background=color_failed)
 
 
 root = TkinterDnD.Tk()
